69497e0e2a04eae6677029822cb079ac7110dae3
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2025-10-15 00:10:34 +0000

    added polling rules

---
+++ moat/src/utils/mod.rs
@@ -0,0 +1,40 @@
+pub mod bpf_utils {
+    use std::net::Ipv4Addr;
+
+    use crate::bpf::{self, FilterSkel};
+
+    pub fn bpf_attach_to_xdp(
+        skel: &mut FilterSkel<'_>,
+        ifindex: i32,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        match skel.progs.firewall.attach_xdp(ifindex) {
+            Ok(link) => {
+                skel.links = bpf::FilterLinks {
+                    firewall: Some(link),
+                };
+
+                Ok(())
+            }
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+
+    pub fn ipv4_to_u32_be(ip: Ipv4Addr) -> u32 {
+        u32::from_be_bytes(ip.octets())
+    }
+
+    pub fn convert_ip_into_bpf_map_key_bytes(ip: Ipv4Addr, prefixlen: u32) -> Box<[u8]> {
+        let ip_u32: u32 = ip.into();
+        let ip_be = ip_u32.to_be();
+
+        let my_ip_key: bpf::types::lpm_key = bpf::types::lpm_key {
+            prefixlen,
+            addr: ip_be,
+        };
+
+        let my_ip_key_bytes = unsafe { plain::as_bytes(&my_ip_key) };
+        my_ip_key_bytes.to_vec().into_boxed_slice()
+    }
+}
+
+pub mod http_utils {}
