1e06475bfdb0f82b4c0ec1c64113f2873df84f38
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2025-10-15 08:39:28 +0000

    renameing of the bpf-firewall project

--- bpf-firewall/src/app_state.rs
+++
@@ -1,8 +0,0 @@
-use crate::{bpf, ssl::SharedTlsState};
-use std::sync::Arc;
-
-#[derive(Clone)]
-pub struct AppState {
-    pub skel: Option<Arc<bpf::FilterSkel<'static>>>,
-    pub tls_state: SharedTlsState,
-}
